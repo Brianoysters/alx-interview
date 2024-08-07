5cae2f673c9d52d0cb79d4225d55b42074073b64
Brianoysters <brianoysters@gmail.com>
2024-08-07 13:59:45 +0000

    minoperand

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for calculating the minimum
+number of operations
+"""
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operand_1 = 0
+    init_length = 1
+    operand_2 = 2
+
+    while n > 1:
+        while n % operand_2 == 0:
+            operand_1 += operand_2
+            n //= operand_2
+        operand_2 += 1
+
+    return operand_1
+
