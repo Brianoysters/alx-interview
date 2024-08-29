d6bcc272f15fc382cb3c1c7b2de0b72013975751
Brianoysters <brianoysters@gmail.com>
2024-08-29 02:34:08 -0700

    queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import sys
+
+def validate_args():
+    """Validates the command-line arguments."""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return N
+
+def is_safe(board, row, col):
+    """Checks if it's safe to place a queen at board[row][col]."""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(N):
+    """Solves the N Queens problem using backtracking."""
+    def backtrack(row, board):
+        if row == N:
+            result.append(board[:])
+            return
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(row + 1, board)
+                board[row] = -1
+
+    result = []
+    board = [-1] * N
+    backtrack(0, board)
+    return result
+
+def print_solutions(solutions):
+    """Prints all solutions in the required format."""
+    for solution in solutions:
+        formatted_solution = [[i, solution[i]] for i in range(len(solution))]
+        print(formatted_solution)
+
+if __name__ == "__main__":
+    N = validate_args()
+    solutions = solve_nqueens(N)
+    print_solutions(solutions)
+
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,1 @@
+A readme file on N Queens project
